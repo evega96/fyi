ab651179739638577ea6ec09f619623671594324
evega96 <144001211+evega96@users.noreply.github.com>
2023-09-15 11:16:56 +0200

    login and register background video

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-import { View, Text, Button, StyleSheet, Image, TextInput, Alert } from "react-native";
+import { View, Animated, Button, StyleSheet, Image, TextInput, Alert } from "react-native";
 
 import tinta from '../../../assets/tinta.mp4'
 import { Video } from 'expo-av';
@@ -11,15 +11,43 @@ import { Video } from 'expo-av';
 
 const LoginButton = ({ navigation }) => {
   const { user, onChangeUser } = React.useState();
-  const { password, onChangePassword } = React.useState();
+  const { password, onChangePassword } = React.useState(); 
+  const [animation] = React.useState(new Animated.Value(0));
+
+  React.useEffect(() => {
+    // Realiza la animación cuando el componente se monta
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 1000, // Duración de la animación en milisegundos
+      useNativeDriver: false, // Ajusta esta propiedad según tus necesidades
+    }).start();
+  }, []);
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex:1}} >
       <Video
-        source={tinta} // o require('ruta/al/video.mp4') si está en tu proyecto
-        style={{ flex: 1 }}
+        source={tinta}
+        style={styles.backgroundVideo}
         isLooping={true}
         shouldPlay={true}
-      >
+        resizeMode="cover"
+      />
+<Animated.View
+      style={[
+        styles.container,
+        {
+          opacity: animation, // Aplicar opacidad según el valor de la animación
+          transform: [
+            {
+              scale: animation.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0.5, 1], // Escala de 0.5 a 1
+              }),
+            },
+          ],
+        },
+      ]}
+    >
+      
         <View style={styles.main}>
           <Image
             style={styles.logo}
@@ -45,8 +73,8 @@ const LoginButton = ({ navigation }) => {
           <Button title="Iniciar sesion" onPress={() => Alert.alert("Boton pulsado iniciar sesion")} />
           <Button title="Registrar" onPress={() => navigation.navigate("Register")} />
         </View>
-      </Video>
-    </View>
+        </Animated.View>
+        </View>
   );
 };
 
@@ -63,7 +91,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   main: {
-    flex: 1,
+    flex:1,
     justifyContent: "center",
   },
   input: {
@@ -72,7 +100,7 @@ const styles = StyleSheet.create({
     height: 25,
     backgroundColor: "#E4E4E4",
     marginBottom: 10,
-    marginLeft: 112,
+    marginLeft: 112
   },
   backgroundVideo: {
     position: 'absolute',
