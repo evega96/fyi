85d508110899a5ac0f64310406e9b2e9bd5ab2ee
evega96 <144001211+evega96@users.noreply.github.com>
2023-10-18 09:33:54 +0200

    errors

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,25 +1,23 @@
 import { initializeApp, getApp } from "firebase/app";
 import {
-    initializeAuth,
-    getAuth,
-    getReactNativePersistence,
+  initializeAuth,
+  getAuth,
+  getReactNativePersistence,
 } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { VITE_PROJECT_ID, VITE_API_KEY } from '@env'
 
 // import {...} from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: VITE_API_KEY,
-    authDomain: VITE_PROJECT_ID + ".firebaseapp.com",
-    databaseURL: VITE_PROJECT_ID + ".firebaseio.com",
-    projectId: VITE_PROJECT_ID,
-    storageBucket: VITE_PROJECT_ID + ".appspot.com",
-    messagingSenderId: "744259670184",
-    appId: "1:744259670184:web:ac316857ea9ffc49a0729d",
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_PROJECT_ID + ".firebaseapp.com",
+  databaseURL: VITE_PROJECT_ID + ".firebaseio.com",
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_PROJECT_ID + ".appspot.com",
+  messagingSenderId: "744259670184",
+  appId: "1:744259670184:web:ac316857ea9ffc49a0729d",
 };
 
 // initialize Firebase App
@@ -27,7 +25,7 @@ const app = initializeApp(firebaseConfig);
 // initialize Firebase Db  and Auth
 export const db = getFirestore();
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 export const storage = getStorage(app);
 export { app, auth, getApp, getAuth };
